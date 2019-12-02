342d0c93d6c3e9ab105bfcf0c79bbdfa6dffafb6
unknown <vineethapidugu444@gmail.com>
2019-12-02 19:49:16 +0530

    commit

---
+++ src/hashing/HashTableExe.java
@@ -0,0 +1,34 @@
+package hashing;
+
+public class HashTableExe {
+
+	public static void main(String[] args) {
+		
+		HashTable table = new HashTable(19);
+		
+		table.insert("Apple");
+		table.insert("Hello");
+		table.insert("Feeling");
+		table.insert("Water");
+		table.insert("Africa");
+		table.insert("Speed");
+		table.insert("Phone");
+		table.insert("September");
+		table.insert("Micheal");
+		table.insert("Milk");
+		table.insert("Huge");
+		table.insert("Dogs");
+		
+        table.displayTable();
+        
+	//	System.out.println("*************FIND ELEMENTS******************");
+	//	System.out.println(table.find("Apple"));
+	//	System.out.println(table.find("Zebra"));
+	//	System.out.println(table.find("Feeling"));
+	//	System.out.println(table.find("Water"));
+	//	System.out.println(table.find("Africa"));
+	//	System.out.println(table.find("Feeling"));
+		
+	}
+
+}
